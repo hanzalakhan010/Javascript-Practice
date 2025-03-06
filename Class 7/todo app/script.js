d448b98291199978ef377a045a2a319389339e3b
Hanzala Khan <pythondeveloper360@gmail.com>
2025-03-06 08:30:25 +0500

    worked on todo app

---
+++ Class 7/todo app/script.js
@@ -0,0 +1,15 @@
+var todos = []
+function addTodo(){
+    let todo = document.getElementById('newTodo').value
+    if (todo){
+        let newTodo = document.createElement('p')
+        newTodo.innerText = todo
+        newTodo.classList.add('todo')
+        document.getElementById('todos').appendChild(newTodo)
+
+        todos.push(newTodo)
+        document.getElementById('newTodo').value = ''
+
+    }
+}
+document.getElementById('addBtn').addEventListener('click',addTodo)
