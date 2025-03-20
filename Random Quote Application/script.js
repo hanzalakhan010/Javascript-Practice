205d0a41d65531a5d0e7bab0bc91b9f5517702f0
Hanzala Khan <pythondeveloper360@gmail.com>
2025-03-20 08:39:13 +0500

    Created Random Quote Application

---
+++ Random Quote Application/script.js
@@ -0,0 +1,26 @@
+let localQuote = JSON.parse(localStorage.getItem('quotes'))
+async function loadQuote() {
+    if (!localQuote) {
+        let response = await fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
+        let data = await response.json()
+        console.log(data.quotes)
+        localStorage.setItem('quotes', JSON.stringify(data.quotes))
+        localQuote = data.quotes
+        // loadQuote()
+    }
+    else {
+        let randomInt = Math.floor(Math.random()*localQuote.length)
+        document.getElementById('text').innerText = localQuote.at(randomInt)['quote']
+        document.getElementById('author').innerText = localQuote.at(randomInt)['author']
+    }
+}
+
+
+async function copyQuote(){
+    let clipBoard = new Clipboard()
+    quote = document.getElementById('text').innerText
+    quote = document.getElementById('autor').innerText
+    await clipBoard.writeText(`Quote:${quote},Autor:${author}`)
+    alert('Copoied to clipboard')
+}
+loadQuote()
