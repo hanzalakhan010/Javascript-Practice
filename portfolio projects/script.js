1af6b1ba76527964f3c43061e8f797b2917dfc8d
Hanzala Khan <pythondeveloper360@gmail.com>
2025-03-26 20:22:35 +0500

    Working on  portfoli project

--- portfolio projects/script.js
+++ portfolio projects/script.js
@@ -1,7 +1,25 @@
-let projects = JSON.parse(localStorage.getItem("project") ?? '{"projects:[]"}');
+var projects = JSON.parse(localStorage.getItem("projects") ?? "[]");
 
 function saveState() {
   localStorage.setItem("projects", JSON.stringify(projects));
+}
+
+crypto
+function loadState() {
+  if (projects) {
+    projects.forEach((project) => {
+      document.getElementById("tbody").insertAdjacentHTML(
+        "beforeend",
+        `
+                  <tr>
+                      <td>${project.projectTitle}</td>
+                      <td>${project.projectDesc}</td>
+                      <td class = 'liveURL'><a target = '__blank' href = '${project.liveUrl}'>Link</a></td>
+                  </tr>
+                  `
+      );
+    });
+  }
 }
 
 function addProject() {
@@ -10,7 +28,26 @@ function addProject() {
   let liveUrl = document.getElementById("liveURL").value;
   if (projectTitle && projectDesc) {
     projects.push({ projectTitle, projectDesc, liveUrl });
+    saveState();
+    document.getElementById("tbody").insertAdjacentHTML(
+      "beforeend",
+      `
+        <tr>
+            <td>${projectTitle}</td>
+            <td>${projectDesc}</td>
+            <td class = 'liveURL'><a target = '__blank' href = '${liveUrl}'>Link</a></td>
+        </tr>
+        `
+    );
   } else {
     alert("Project Title and project description can\t be empty");
   }
 }
+
+document.getElementById("addBtn").addEventListener("click", (event) => {
+  event.preventDefault();
+  addProject();
+});
+document.addEventListener("DOMContentLoaded", () => {
+  loadState();
+});
