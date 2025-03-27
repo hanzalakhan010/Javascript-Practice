edfd1f4f7b476216b70a790241cfacf8cdd9a74f
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-03-27 05:51:12 +0000

    class 11

---
+++ class 11/script.js
@@ -0,0 +1,40 @@
+const colours = ['red', 'green']
+const favColour = ['orange', 'purple']
+const best = 'white'
+
+const allColours = [...colours, best, ...favColour]
+// console.log(allColours)
+
+
+const user = {
+    id: 1,
+    name: "hanzala",
+    email: 'h@g.com',
+    age: 10
+
+}
+
+const {email,id:userId} = user
+// console.log(userId)
+// console.log(email)
+
+
+
+const updatedUser = { ...user, email: 'hanzala@g.com', age: 12 }
+// console.log(updatedUser)
+
+
+function mergeArray(arr1, arr2) {
+    return [...arr1, ...arr2]
+}
+
+
+function sum(...args) {
+    return args.reduce((ele, curr) => ele + curr)
+}
+
+
+// console.log(sum(1, 2, 3, 4))
+
+
+
